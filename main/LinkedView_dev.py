4d1588cba63e8682af15a2d4155490f45bc7fe00
elainehoml <eml.ho@soton.ac.uk>
2020-07-15 23:37:29 +0100

    Match window size, Export view
    Matches window sizes for all views, export views as tiffs
    
    - Image class has new method matchWindowSizes which matches all window dimensions to the reference image. Strange bug when the reference image is made larger than other images, the window size updates but the image does not zoom out to fill the entire window.
    
    - Changed 'target_image' to 'reference_image'
    
    - LinkedView has new method exportView which exports current views to individual tiffs, saved in same directory as reference image (LV_Export/<current datetime>). If any ROIs are marked they are also saved.

--- main/LinkedView_dev.py
+++ main/LinkedView_dev.py
@@ -1,7 +1,9 @@
-import os, time
+import os, time, datetime
 from ij import IJ, ImagePlus
 from ij import WindowManager as WM
 from ij.gui import ImageCanvas, GenericDialog
+from ij.io import FileSaver
+from ij.plugin import ScreenGrabber
 from javax.swing import JFrame, JButton
 from java.awt import GridLayout
 
@@ -18,6 +20,8 @@ class Image():
 		Unique numeric ID assigned to this imp
 	canvas : ImageCanvas
 		ImageCanvas for image
+	window : ImageWindow
+		ImageWindow for image
 	
 	"""
 	def __init__(self, imp):
@@ -25,11 +29,27 @@ class Image():
 		self.title = self.imp.getTitle()
 		self.ID = self.imp.getID()
 		self.canvas = self.imp.getCanvas()
+		self.window = self.imp.getWindow()
 		IJ.log("Loaded image " + self.imp.getTitle())
 
+	def getWindowSize(self):
+		""" Gets window size of current window from ImageWindow """
+		return self.window.getSize() # java.awt.Dimension object [width, height]
+	
 	def getView(self):
 		""" Gets source rectangle of current view from ImageCanvas """
 		return self.canvas.getSrcRect() # java.awt.Rectangle object
+
+	def matchWindowSizes(self, ref_window_size):
+		""" Matches window dimensions to the reference image
+
+		Parameters
+		----------
+		ref : java.awt.Dimension
+			Dimension [width, height] of ImageWindow to resize to
+		
+		"""
+		self.window.setSize(ref_window_size)
 	
 	def updateView(self, ref):
 		""" Sets display to ref src_rect
@@ -57,7 +77,7 @@ class LinkedView():
 		Images[ID], where ID is the unique image identifier for each ImagePlus.
 	ref_image : Image
 		Image instance used as ref display, i.e. all other Image instances
-		will be matched to the same source rect as this target_image.	
+		will be matched to the same source rect as this ref_image.	
 
 	"""
 	def __init__(self):
@@ -78,9 +98,9 @@ class LinkedView():
 			pass
 
 	def getRef(self, event):
-		""" Dialog to update ref image attribute when get_target button is pressed """
+		""" Dialog to update ref image attribute when get_ref button is pressed """
 		self.checkImages()
-		gd = GenericDialog("Choose a target image")
+		gd = GenericDialog("Choose a reference image")
 		gd.addChoice("Reference image", 
 			[Image.title for Image in self.Images.values()],
 			self.Images.values()[0].title) # add all Image instances to a drop-down list
@@ -96,19 +116,51 @@ class LinkedView():
 		is pressed
 		"""
 		self.checkImages()
+		ref_window_size = self.ref_image.getWindowSize() # java.awt.Dimension object
 		ref_src_rect = self.ref_image.getView() # java.awt.Rectangle object
 		for Image in self.Images.values(): # updates all Image instances
 			Image.updateView(ref_src_rect)
+			Image.matchWindowSizes(ref_window_size) # why does this not work when making images larger
 
+	def exportView(self, event):
+		""" Export current view of all images as individual tiffs when 
+		export_view button is pressed
+
+		1. Export directory is created in same dir as ref image
+		2. Folder created for specific time
+		3. ScreenGrabber returns current views of all images to be saved
+
+		These images can be loaded into FigureJ for figure making
+		"""
+		# Create export dir, if not already present
+		IJ.selectWindow(self.ref_image.ID) # make ref image current active window
+		current_datetime = datetime.datetime.now().strftime("%Y%m%d_%H%M")
+		export_dir = os.path.join(IJ.getDir("image"), "LV_export")
+		if os.path.isdir(export_dir) == False:
+			IJ.log("Created export directory: " + export_dir)
+			os.mkdir(export_dir)
+		current_export_dir = os.path.join(export_dir, current_datetime)
+		os.mkdir(current_export_dir)
+		IJ.log("Currently exporting to: " + current_export_dir)
+
+		# Save image
+		for Image in self.Images.values(): # for all Image instances
+			IJ.selectWindow(Image.ID)
+			screengrab = ScreenGrabber().captureImage()
+			FileSaver(screengrab).saveAsTiff(os.path.join(export_dir, current_datetime, Image.title))
+			IJ.log("Image saved: " + os.path.join(current_export_dir, Image.title))
+	
 	def UI(self):
 		""" User interface for LinkedView """
 		frame = JFrame("LinkedView")
-		frame.setSize(300,100)
-		frame.setLayout(GridLayout(2,1))
+		frame.setSize(300,200)
+		frame.setLayout(GridLayout(3,1))
 		link_view = JButton("Link Views", actionPerformed = self.linkView)
 		get_ref = JButton("Update Reference Image", actionPerformed = self.getRef)
+		export_view = JButton("Export Views", actionPerformed = self.exportView)
 		frame.add(link_view)
 		frame.add(get_ref)
+		frame.add(export_view)
 		frame.setVisible(True)
 
 launch = LinkedView()
